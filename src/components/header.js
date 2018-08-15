9255dd9ee47c7e65fdaca846fc1b112bcd3bc3bb
NBK-14-011 <NBK-14-011@NBK-14-011>
2018-08-15 16:21:20 +0800

    commit

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React, {Component} from '../../../../../../Users/NBK-14-011/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react';
+
+export default class Header extends Component {
+    render(){
+        return(
+            <h1>Todo App </h1>
+        );
+    }
+}
